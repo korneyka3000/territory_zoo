0c33344d64aacb05f13429746419ea68008335a9
oleg <olegpustovalov220@gmail.com>
2022-07-08 22:15:57 +0300

    fix bug and add image from models+

--- ZOO/settings.py
+++ ZOO/settings.py
@@ -157,7 +157,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CKEDITOR_UPLOAD_PATH = ''
-CKEDITOR_RESTRICT_BY_DATE = True
 CKEDITOR_CONFIGS = {
     'default': {
         'enterMode': 2,
@@ -165,7 +164,7 @@ CKEDITOR_CONFIGS = {
         'toolbar_YourCustomToolbarConfig': [
             {'name': 'paragraph',
              'items': ['NumberedList', 'BulletedList',
-                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock',]},
+                       'JustifyLeft', 'JustifyCenter', 'JustifyRight', 'JustifyBlock']},
         ],
         'toolbar': 'YourCustomToolbarConfig',
         'tabSpaces': 4,
